e5d31e740d94b19ab41dd6128a17ffe409e31456
makepositivechange <brshankar1992@gmail.com>
2025-06-01 10:13:56 +0200

    added 2 get endpoints

--- database/database.go
+++ database/database.go
@@ -4,15 +4,14 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func MySQLConn(ctx context.Context) (*gorm.DB, error) {
+func MySQLConn(ctx context.Context, username string, password string, host string, port string, dbname string) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("USER"), os.Getenv("PASSWORD"), os.Getenv("HOST"), os.Getenv("PORT"), os.Getenv("DATABASE_NAME"))
+		username, password, host, port, dbname)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Printf("Error while connecting to database:%v", err)
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/makepostivechange/cats-echo-project/database"
 	"github.com/makepostivechange/cats-echo-project/handler"
+	"github.com/makepostivechange/cats-echo-project/models"
 )
 
 func init() {
@@ -22,13 +23,21 @@ func init() {
 func main() {
 	e := echo.New()
 	// All the endpoints will be defined here
-	db, err := database.MySQLConn(context.Background())
+	db, err := database.MySQLConn(context.Background(), os.Getenv("USERNAME"), os.Getenv("PASSWORD"), os.Getenv("HOST"), os.Getenv("PORT"), os.Getenv("DATABASE_NAME"))
 	if err != nil {
 		log.Printf("Error while connecting to the database:%v", err)
 		return
 	}
-	_ = handler.Handler{ // Don't forget to declare variable
+	var cat_model models.CatInfo
+	err = db.AutoMigrate(&cat_model)
+	if err != nil {
+		log.Printf("Error while creating table in database:%v", err)
+	}
+	h := handler.Handler{ // Don't forget to declare variable
 		DB: db,
 	}
+	e.GET("/health", h.HealthCheck)
+	e.GET("/cats", h.GetCats)
+	e.GET("/cats/breed_name/:breed_name", h.GetCat)
 	e.Logger.Fatal(e.Start(os.Getenv("APP_PORT")))
 }
