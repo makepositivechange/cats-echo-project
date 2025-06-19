ab1062282b9d306722dde0b291b7f0f10654b952
makepositivechange <brshankar1992@gmail.com>
2025-06-19 10:33:37 +0200

    improved database file implementation

--- database/database.go
+++ database/database.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+// Setting connection poll const
+const POOL = 25
 
 func MySQLConn(ctx context.Context, username string, password string, host string, port string, dbname string) (*gorm.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
@@ -17,6 +21,31 @@ func MySQLConn(ctx context.Context, username string, password string, host strin
 		log.Printf("Error while connecting to database:%v", err)
 		return nil, err
 	}
-	log.Println("Connected successfully to DB")
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Printf("Error while getting database object:%v", err)
+		return nil, err
+	}
+	// Setting connection pool setting
+	sqlDB.SetMaxOpenConns(POOL)
+	sqlDB.SetMaxIdleConns(POOL)
+	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+
+	// Pinging to verify connection timeout
+	pingCh := make(chan error, 1)
+	go func() {
+		pingCh <- sqlDB.PingContext(ctx)
+	}()
+	select {
+	case <-ctx.Done():
+		log.Printf("Database ping timed out:%v", ctx.Err())
+		return nil, ctx.Err()
+	case err := <-pingCh:
+		if err != nil {
+			log.Printf("Error pinging database:%v", err)
+			return nil, err
+		}
+
+	}
 	return db, nil
 }
